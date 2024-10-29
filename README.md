baf8c3df701d647509291a5831a2e8bf12f4399c
Mustafa ÖZKAYA <mustafaozkaya4221@gmail.com>
2024-10-30 00:18:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Smart-City-App
+ Smart City Application Developed for İzmir Smart City Hackathon in 2024 with Flutter. 
---
+++ android/app/src/main/kotlin/com/example/akillisehir/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.akillisehir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
